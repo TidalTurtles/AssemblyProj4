200420ffd86bd52f4af6697d23d28670a2e7332c
Noah Holt <tidalturtles@gmail.com>
2022-07-26 14:31:57 -0600

    fixed the connection
    fixed the connection issue between the c and assembly program. And the assembly program runs an issue when dividing.

---
+++ getMean.s
@@ -0,0 +1,30 @@
+
+.text
+
+.global getMean_
+
+#step one add all values in rdi, rsi, rdx, rcx
+
+getMean_:
+
+  mov %rdi, %rax  #put first value in eax(return position) so you don't forget
+
+  #now add the rest
+
+  add %rsi, %rax  #add a + b
+
+  add %rdx, %rax  #add a+b+c
+
+  add %rcx, %rax  #add a+b+c+d
+
+  #finish adding now will need to divide by 4
+  #idiv on linux uses specified registers
+  # idiv = eax / specified
+
+#  mov $4, %ecx
+#  idiv %ecx 
+
+  #result is in eax so done
+
+  ret
+
